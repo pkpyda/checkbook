8a1cb84b9da4077df3f4bd22462ca6d04946a8a9
pkpyda <pydapraveenkumar@gmail.com>
2014-07-14 22:56:45 +0100

    scaffolded status

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Checkbook::Application.config.secret_token = 'da5569d83f6aa2fb1c9ed4c89067ca6e8fbef0574a708d81cfc71dc2c3ed02205ad58c1aed22ac5989aee0ff769adc0e45ae004ccbdda4714ad1f803aa4a7f1f'
